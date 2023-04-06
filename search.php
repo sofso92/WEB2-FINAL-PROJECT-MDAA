e91e08be7b6b24d01ac19e1c72a03ae1d269b346
ss <soriano.sofia.v@gmail.com>
2023-04-06 08:33:22 -0500

    added functionality to check 'search' and 'submit' using

--- search.php
+++ search.php
@@ -1,5 +1,19 @@
 <?php
-    $echo = 'work in progress';
+    require 'partials/header.php';
+
+    if (isset($_GET['search']) && isset($_GET['submit'])) {
+        $search = filter_var($_GET['search'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    
+        $query = "SELECT * FROM posts WHERE title LIKE :search ORDER BY date_time DESC";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindValue(':search', '%' . $search . '%', PDO::PARAM_STR);
+        $stmt->execute();
+    
+        $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } else {
+        header('location: ' . ROOT_URL . 'blog.php');
+        die();
+    }
 ?>
 <html>  
     <h1>
