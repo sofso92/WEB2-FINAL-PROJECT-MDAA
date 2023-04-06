e1a90994593f7b2c870355810563eda4b9ee249d
ss <soriano.sofia.v@gmail.com>
2023-04-06 08:53:45 -0500

    included logic to fetch posts and their related categories and authors from result set

--- search.php
+++ search.php
@@ -15,8 +15,75 @@
         die();
     }
 ?>
-<html>  
-    <h1>
-    <?=$echo?>
-    <h1>
-</html>
+
+<?php if (mysqli_num_rows($posts) > 0) :?>
+    <section class="posts section__extra-margin">
+        <div class="container posts__container">
+            <?php
+            while ($post = $posts->fetch(PDO::FETCH_ASSOC)) :
+                ?>
+                <article class="post">
+                    <div class="post__thumbnail">
+                        <img src="./images/<?= $post['thumbnail'] ?>">
+                    </div>
+                    <div class="post__info">
+                        <?php
+                        // fetch category from categories table using category_id of post
+                        $category_id = $post['category_id'];
+                        $category_query = "SELECT * FROM categories WHERE id=:category_id";
+                        $category_stmt = $pdo->prepare($category_query);
+                        $category_stmt->execute(array(':category_id' => $category_id));
+                        $category = $category_stmt->fetch(PDO::FETCH_ASSOC);
+                        ?>
+                        <a href="<?= ROOT_URL ?>category-posts.php?id=<?= $post['category_id'] ?>" class="category__button"><?= $category['title'] ?></a>
+                        <h3 class="post__title">
+                            <a href="<?= ROOT_URL ?>post.php?id=<?= $post['id'] ?>"><?= $post['title'] ?></a>
+                        </h3>
+                        <p class="post__body">
+                            <?= substr($post['body'], 0, 150) ?>...
+                        </p>
+                        <div class="post__author">
+                            <?php
+                            // fetch author from users table using author_id
+                            $author_id = $post['author_id'];
+                            $author_query = "SELECT * FROM users WHERE id=:author_id";
+                            $author_stmt = $pdo->prepare($author_query);
+                            $author_stmt->execute(array(':author_id' => $author_id));
+                            $author = $author_stmt->fetch(PDO::FETCH_ASSOC);
+                            ?>
+                            <div class="post__author-avatar">
+                                <img src="./images/<?= $author['avatar'] ?>">
+                            </div>
+                            <div class="post__author-info">
+                                <h5>By: <?= "{$author['firstname']} {$author['lastname']}" ?></h5>
+                                <small>
+                                    <?= date("M d, Y - H:i", strtotime($post['date_time'])) ?>
+                                </small>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+            <?php endwhile ?>
+        </div>
+    </section>
+<?php else : ?>
+    <div class="alert__message error lg section__extra-margin">
+        <p>No posts found for this search</p>
+    </div>
+<?php endif ?>
+
+<section class="category__buttons">
+    <div class="container category__buttons-container">
+        <?php
+        $all_categories_query = "SELECT * FROM categories";
+        $all_categories_stmt = $pdo->query($all_categories_query);
+        while ($category = $all_categories_stmt->fetch(PDO::FETCH_ASSOC)) :
+            ?>
+            <a href="<?= ROOT_URL ?>category-posts.php?id=<?= $category['id'] ?>" class="category__button"><?= $category['title'] ?></a>
+        <?php endwhile ?>
+    </div>
+</section>
+
+<?php include 'partials/footer.php' ?>
+
+
